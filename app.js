c8ac9a9a02476c9b8ce66077d2752aaaa5619a2e
Jiali Lu <jialilyv@gmail.com>
2022-03-31 18:30:45 +0800

    change port

--- app.js
+++ app.js
@@ -32,6 +32,6 @@ app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
